fc69b19fd76d705777f57a660de404dedecf991c
Mauricio Manoel <mauriciomanoel@gmail.com>
2022-01-18 00:55:43 +0000

    remove input unused

--- app/src/main/java/com/mauricio/dogapichallenger/di/module/NetworkModule.kt
+++ app/src/main/java/com/mauricio/dogapichallenger/di/module/NetworkModule.kt
@@ -11,12 +11,11 @@ import okhttp3.Cache
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
-//import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.TimeUnit
 import javax.inject.Singleton
 
-@Module(includes = [AppModule::class])
+@Module
 object NetworkModule {
 
     const val BASE_URL = "https://api.thedogapi.com/"
